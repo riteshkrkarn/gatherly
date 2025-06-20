6d4be35971c33589ba7c8df1ee892fc795b29c6f
Ritesh <riteshkumarkarn414@gmail.com>
2025-06-20 23:13:56 +0530

    Enhance user registration process in sign-up route
    - Added verification code generation and expiry handling for users during registration.
    - Updated existing user handling to include password reset and verification code assignment.
    - Implemented email verification functionality with appropriate success and error responses.
    - Improved response messages for user registration outcomes.

--- src/app/api/sign-up/routes.ts
+++ src/app/api/sign-up/routes.ts
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 import { success } from "zod/v4";
 import UserModel from "@/model/User.model";
+import { TURBOPACK_CLIENT_MIDDLEWARE_MANIFEST } from "next/dist/shared/lib/constants";
 
 export async function POST(request: Request) {
   await dbConnect();
@@ -26,23 +27,70 @@ export async function POST(request: Request) {
     }
 
     const existingUserByEmail = await UserModel.findOne({ email });
+
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
     if (existingUserByEmail) {
-      true;
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: true,
+            message: "User is already registered with this email.",
+          },
+          { status: 401 }
+        );
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = hashedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save();
+      }
     } else {
       const hashedPassword = await bcrypt.hash(password, 10);
       const expiryDate = new Date();
       expiryDate.setHours(expiryDate.getHours() + 1);
 
-      new UserModel({
+      const newUser = new UserModel({
         name,
         email,
         userName,
         isOrganizer,
         avatar: "",
         password: hashedPassword,
-        verifyCodeExpiry: Date,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
       });
+
+      await newUser.save();
     }
+
+    //Send verification email
+    const emailResponse = await sendVerificationEmail(
+      email,
+      userName,
+      verifyCode
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User regiestered. Please verify your email.",
+      },
+      {
+        status: 201,
+      }
+    );
   } catch (error) {
     console.log("Error registering user.");
     return Response.json(
